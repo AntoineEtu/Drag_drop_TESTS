abc37d0c61acaf9d0ba5785945e63bd3f788ba30
Unknown <antoinekeribin@hotmail.fr>
2017-12-21 15:46:10 +0100

    truc

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -1,12 +1,16 @@
-document.querySelector('.film_connexion_structure').addEventListener('dragover', function(e) {
-    e.preventDefault(); // Annule l'interdiction de drop
-});
+$(document).ready(function () {
 
-var draggableElement =document.querySelector('.film_connexion_structure').addEventListener('drop', function(e) {
-    e.preventDefault(); // Cette méthode est toujours nécessaire pour éviter une éventuelle redirection inattendue
-    console.log('success');
-});
+	document.querySelector('.film_connexion_structure').addEventListener('dragover', function(e) {
+	    e.preventDefault(); // Annule l'interdiction de drop
+	});
 
-draggableElement.addEventListener('dragstart', function(e) {
-    e.dataTransfer.setData('text/plain', "Ce texte sera transmis à l'élément HTML de réception");
+	document.querySelector('.film_connexion_structure').addEventListener('drop', function(e) {
+	    e.preventDefault(); // Cette méthode est toujours nécessaire pour éviter une éventuelle redirection inattendue
+	    console.log('success');
+	});
+
+	document.querySelector('.film_added_element').addEventListener('dragstart', function(e) {
+	    e.dataTransfer.setData('text/plain', "Ce texte sera transmis à l'élément HTML de réception");
+	});
+
 });
