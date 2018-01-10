f526c7d13eb4fa228ad0baeaa01813a5e82d96a8
Unknown <antoinekeribin@hotmail.fr>
2018-01-10 15:15:03 +0100

    Modifs

--- js/structure.js
+++ js/structure.js
@@ -1,16 +1,18 @@
-define([ "jquery", "plugins/jquery.panzoom" ], function( $ ) {
-  $(document).ready(function() {
-    $(".panzoom-elements").panzoom();
-  });
-});
-
-
 $(document).ready(function () {
-	$(".panzoom-elements").panzoom();
+	$elem = $(".panzoom-elements").panzoom();
 
 	// Pass options
 	$("a.panzoom-elements").panzoom({
 	  minScale: 0,
 	  $zoomRange: $("input[type='range']")
 	});
+	// Several options at once
+	$elem.panzoom("option", {
+	  increment: 0.4,
+	  minScale: 0.1,
+	  maxScale: 2,
+	  duration: 500,
+	  $reset: $("a.reset-panzoom, button.reset-panzoom")
+	});
 });
+
