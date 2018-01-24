0c82eef075eee3ca59f111239cd23f9ae6f72b16
Unknown <antoinekeribin@hotmail.fr>
2018-01-24 10:14:19 +0100

    Zoom cassé (à revoir)

--- js/structure.js
+++ js/structure.js
@@ -1,18 +1,11 @@
 $(document).ready(function () {
-	$elem = $(".panzoom-elements").panzoom();
-
-	// Pass options
-	$("a.panzoom-elements").panzoom({
-	  minScale: 0,
-	  $zoomRange: $("input[type='range']")
-	});
-	// Several options at once
-	$elem.panzoom("option", {
-	  increment: 0.4,
-	  minScale: 0.1,
-	  maxScale: 2,
-	  duration: 500,
-	  $reset: $("a.reset-panzoom, button.reset-panzoom")
+	$elem = $(".panzoom-elements");
+	
+	var panzoom = $elem.panzoom('instance');
+	var options = $elem.panzoom('option');
+	$elem.panzoom('option', {
+		duration: 500,
+		easing: 'linear'
 	});
 });
 
